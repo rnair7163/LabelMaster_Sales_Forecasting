602cf3c39b112ae0bd7d05a8cf456012aff16844
Omkar Pawar <omkarpawar@Omkars-MacBook-Pro.local>
2021-04-13 19:58:01 -0500

    Web Page now shows plot

--- UI/app.py
+++ UI/app.py
@@ -2,8 +2,14 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
-import data_prep as dp
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
+import matplotlib.pyplot as plt
+import os
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import io
+import random
+from flask import Response
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -26,10 +32,7 @@ class Item(object):
 def home():
     return render_template('index.html')
 
-
-@app.route('/predict',methods=['POST'])
-def predict():
-
+def model_data():
     books = pd.read_csv('/Users/omkarpawar/Downloads/Books_ext_fs.csv')
     books = books.dropna(axis = 1)
     sales = books[['Year_Month','Sum of Sales']]
@@ -37,17 +40,35 @@ def predict():
     train = df[:int(0.8*(len(sales)))]
     test = df[int(0.8*(len(sales))):]
 
+    #output = model.forecast(len(test))
     model = ExponentialSmoothing((train["Sum of Sales"]), trend="add", seasonal="mul", seasonal_periods=12)
     fit = model.fit()
     output = fit.forecast(len(test))
+    return test,output
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    test, output = model_data()
     items = []
     for i in output.index:
-        items.append(Item(i,output[i]))
-
+    	items.append(Item(i,output[i]))
     table = ItemTable(items)
-
     return render_template('index.html', prediction_text=table)
 
+@app.route('/plot',methods=['POST'])
+def plot():
+    test, output = model_data()
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    axis.plot(output.values)
+    axis.plot(test.values)
+    axis.set_title('Forecast vs Actual Values')
+    axis.set_xlabel('TimeLine')
+    axis.set_ylabel('Total Sales')
+    result = io.BytesIO()
+    FigureCanvas(fig).print_png(result)
+    return Response(result.getvalue(), mimetype='image/png')
 
 @app.route('/update',methods=['POST'])
 def update():
@@ -56,26 +77,6 @@ def update():
     external = dp.external_database()
     data = dp.final_data(books, external)
     return data
-
-@app.route('/plot.png',methods=['POST'])
-def chartTest():
-    plt.plot(output.values)
-    plt.plot(test.values)
-    plt.title("Comparision of Actual Sales with Predictions")
-    plt.xlabel("TimeLine")
-    plt.ylabel("Sales")  
-    plt.savefig("templates/Sales.png")
-    return render_template('index.html', name = plt.show(), url ='templates/Sales.png')
-
-@app.route('/results',methods=['POST'])
-def results():
-
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
