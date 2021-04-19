c5622e720439d34706ea301ad0be3d0ddef306bf
rahulmnair1997 <rahulmnair1997@gmail.com>
2021-04-19 07:43:29 -0500

    Added update dataset functionality

--- UI/app.py
+++ UI/app.py
@@ -3,11 +3,13 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
+import data_prep as dp
 import matplotlib.pyplot as plt
 import os
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import io
+import base64
 import random
 from flask import Response
 
@@ -28,12 +30,14 @@ class Item(object):
         self.date = date
         self.sales = sales"""
 
+
 @app.route('/books')
 def home():
     return render_template('index.html')
 
+
 def model_data():
-    books = pd.read_csv('/Users/omkarpawar/Downloads/Books_ext_fs.csv')
+    books = pd.read_csv('books_data.csv')
     books = books.dropna(axis = 1)
     sales = books[['Year_Month','Sum of Sales']]
     df = sales.sort_values("Year_Month").set_index("Year_Month")
@@ -46,9 +50,11 @@ def model_data():
     output = fit.forecast(len(test))
     return test,output
 
+
 @app.route('/')
 def welcome():
     return render_template('welcome.html')
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -62,6 +68,7 @@ def predict():
     #table = ItemTable(items)
     return render_template('index.html', headings=headings,data = data)
 
+
 @app.route('/plot',methods=['POST'])
 def plot():
     test, output = model_data()
@@ -74,16 +81,21 @@ def plot():
     axis.set_ylabel('Total Sales')
     axis.legend()
     result = io.BytesIO()
+
     FigureCanvas(fig).print_png(result)
     return Response(result.getvalue(), mimetype='image/png')
+
 
 @app.route('/update',methods=['POST'])
 def update():
     main_data = dp.main_data_transform()
     books = dp.books_data(main_data)
-    external = dp.external_database()
-    data = dp.final_data(books, external)
-    return data
+    # external = dp.external_database()
+    # data = dp.final_data(books, external)
+    books.to_csv('books_data.csv')
+    statement = 'Books data has been updated'
+    return render_template('index.html', statement=statement)
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'), 
