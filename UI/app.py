1e60f14bbf03e1a3a99ba422576531d84edb6917
rahulmnair1997 <rahulmnair1997@gmail.com>
2021-04-20 12:38:02 -0500

    added table and plot for sales forecasting values

--- UI/app.py
+++ UI/app.py
@@ -1,5 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+from datetime import datetime
+import dateutil.relativedelta
 import pickle
 import pandas as pd
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
@@ -14,8 +16,8 @@ import random
 from flask import Response
 
 app = Flask(__name__)
+model = pickle.load(open('sarima_model.pkl', 'rb'))
 
-from flask_table import Table, Col
 
 @app.route('/books')
 def home():
@@ -25,48 +27,54 @@ def home():
 def model_data():
     books = pd.read_csv('books_data.csv')
     books = books.dropna(axis = 1)
-    sales = books[['Year_Month','Sum of Sales']]
-    df = sales.sort_values("Year_Month").set_index("Year_Month")
-    train = df[:int(0.8*(len(sales)))]
-    test = df[int(0.8*(len(sales))):]
-    model = ExponentialSmoothing((train["Sum of Sales"]), trend="add", seasonal="mul", seasonal_periods=12)
-    fit = model.fit()
-    output = fit.forecast(len(test))
-    return test,output
+    sales = books['Sum of Sales']
+    date = books['Year_Month']
+    date = pd.to_datetime(date)
+    pred_uc = model.get_forecast(steps=12)
+    pred_ci = pred_uc.conf_int()
+    pred_unscaled_ind = pred_uc.predicted_mean
+    pred_unscaled = pred_unscaled_ind .reset_index(drop=True)
+    return date, pred_unscaled, sales, pred_ci, pred_unscaled_ind
 
 
 @app.route('/')
 def welcome():
     return render_template('welcome.html')
 
+
+def future_dates(date):
+    future = []
+    for i in range(1,13):
+        future.append(datetime.strftime(date.iloc[-1] + dateutil.relativedelta.relativedelta(months=i), '%Y-%m'))
+    return future
+
+
 @app.route('/predict',methods=['POST'])
 def predict():
-
-    test, output = model_data()
-    items = []
+    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_data()
+    future = future_dates(date)
     headings = ("Date", "Total Sales ($)")
     data = []
-    for i in output.index:
-    	data.append([i.strftime("%Y-%m"),round(output[i],3)])
-    #table = ItemTable(items)
-    return render_template('index.html', headings=headings,data = data)
+    for i in range(len(future)):
+        data.append([future[i],round(pred_unscaled[i],3)])
+    return render_template('index.html', headings=headings, data=data)
 
 
 @app.route('/plot',methods=['POST'])
 def plot():
-    test, output = model_data()
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    axis.plot(output.values, label = "Forecast")
-    axis.plot(test.values, label = "Actual")
-    axis.set_title('Forecast vs Actual Values')
-    axis.set_xlabel('TimeLine')
-    axis.set_ylabel('Total Sales')
-    axis.legend()
-    result = io.BytesIO()
-
-    FigureCanvas(fig).print_png(result)
-    return Response(result.getvalue(), mimetype='image/png')
+    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_data()
+    fig, ax = plt.subplots()
+    ax = sales.plot(label='observed', figsize=(14, 7))
+    pred_unscaled_ind.plot(ax=ax, label='Forecast')
+    ax.fill_between(pred_ci.index,
+                    pred_ci.iloc[:, 0],
+                    pred_ci.iloc[:, 1], color='k', alpha=.25)
+    ax.set_xlabel('Number of Observations')
+    ax.set_ylabel('Sales')
+    plt.legend()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 
 @app.route('/update',methods=['POST'])
