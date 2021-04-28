6daf5aae53a6982bebf9f6628ee01b98c3accec3
Omkar Pawar <omkarpawar@Omkars-MacBook-Pro.local>
2021-04-27 22:16:52 -0500

    Removed Redundancy and fixed app crash

--- UI/refactored_app.py
+++ UI/refactored_app.py
@@ -58,25 +58,11 @@ def updatePackaging():
     statement = 'Packaging data has been updated'
     return render_template('packaging.html', statement=statement)
 
-
-def model_books_data(filepath):
-    books = pd.read_csv(filepath)
-    books = books.dropna(axis = 1)
-    sales = books['Sum of Sales']
-    date = books['Year_Month']
-    date = pd.to_datetime(date)
-    pred_uc = model.get_forecast(steps=12)
-    pred_ci = pred_uc.conf_int()
-    pred_unscaled_ind = pred_uc.predicted_mean
-    pred_unscaled = pred_unscaled_ind .reset_index(drop=True)
-    return date, pred_unscaled, sales, pred_ci, pred_unscaled_ind
-
-
-def model_package_data(filepath):
-    pack = pd.read_csv(filepath)
-    pack = pack.dropna(axis = 1)
-    sales = pack['Sum of Sales']
-    date = pack['Year_Month']
+def model_data(filepath):
+    data = pd.read_csv(filepath)
+    data = data.dropna(axis = 1)
+    sales = data['Sum of Sales']
+    date = data['Year_Month']
     date = pd.to_datetime(date)
     pred_uc = model_package.get_forecast(steps=12)
     pred_ci = pred_uc.conf_int()
@@ -85,8 +71,9 @@ def model_package_data(filepath):
     return date, pred_unscaled, sales, pred_ci, pred_unscaled_ind
 
 
+
 def books_forecast(filepath):
-    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_books_data(filepath)
+    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_data(filepath)
     future = future_dates(date)
     headings = ("Date", "Total Sales ($)")
     data = []
@@ -96,7 +83,7 @@ def books_forecast(filepath):
 
 
 def package_forecast(filepath):
-    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_package_data(filepath)
+    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_data(filepath)
     future = future_dates(date)
     headings = ("Date", "Total Sales ($)")
     data = []
@@ -111,38 +98,24 @@ def future_dates(date):
         future.append(datetime.strftime(date.iloc[-1] + dateutil.relativedelta.relativedelta(months=i), '%Y-%m'))
     return future
 
-
-def books_plot(filepath):
-    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_books_data(filepath)
-    fig, ax = plt.subplots()
-    ax = sales.plot(label='observed', figsize=(14, 7))
-    pred_unscaled_ind.plot(ax=ax, label='Forecast')
+def plot(filepath):
+    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_data(filepath)
+    fig = Figure(figsize=(25,15))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(sales, label='Observed', linewidth=5)
+    ax.plot(pred_unscaled_ind, label='Forecast', linewidth=5)
     ax.fill_between(pred_ci.index,
                     pred_ci.iloc[:, 0],
                     pred_ci.iloc[:, 1], color='k', alpha=.25)
-    ax.set_xlabel('Number of Observations')
-    ax.set_ylabel('Sales')
-    plt.legend()
+    ax.set_title('Forecast for Next 12 months', fontsize=25)
+    ax.set_xlabel('Number of Observations', fontsize=20)
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(20)
+    ax.set_ylabel('Sales', fontsize=20)
+    ax.legend(prop={"size":20})
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
-
-
-def package_plot(filepath):
-    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_package_data(filepath)
-    fig, ax = plt.subplots()
-    ax = sales.plot(label='observed', figsize=(14, 7))
-    pred_unscaled_ind.plot(ax=ax, label='Forecast')
-    ax.fill_between(pred_ci.index,
-                    pred_ci.iloc[:, 0],
-                    pred_ci.iloc[:, 1], color='k', alpha=.25)
-    ax.set_xlabel('Number of Observations')
-    ax.set_ylabel('Sales')
-    plt.legend()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
 
 @app.route('/predictBooks',methods=['POST'])
 def predictBooks():
@@ -161,16 +134,14 @@ def predictPackaging():
 @app.route('/plotBooks',methods=['POST'])
 def plotBooks():
     books_data_filepath = "books_data.csv"
-    return books_plot(books_data_filepath)
+    return plot(books_data_filepath)
 
 
 @app.route('/plotPackaging',methods=['POST'])
 def plotPackaging():
     packaging_data_filepath = "packaging_data.csv"
-    return package_plot(packaging_data_filepath)
+    return plot(packaging_data_filepath)
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP', '0.0.0.0'), 
-            port=int(os.getenv('PORT', 4426)),debug=True)
-
+    app.run()
