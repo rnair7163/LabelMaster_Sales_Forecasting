5e3c3f09c93a19546d6894226b135554f0dafd17
rahulmnair1997 <rahulmnair1997@gmail.com>
2021-04-28 09:35:10 -0500

    modified plotBooks

--- UI/refactored_app.py
+++ UI/refactored_app.py
@@ -64,12 +64,17 @@ def model_data(filepath):
     sales = data['Sum of Sales']
     date = data['Year_Month']
     date = pd.to_datetime(date)
-    pred_uc = model_package.get_forecast(steps=12)
-    pred_ci = pred_uc.conf_int()
-    pred_unscaled_ind = pred_uc.predicted_mean
-    pred_unscaled = pred_unscaled_ind .reset_index(drop=True)
+    if filepath == 'books_data.csv':
+        pred_uc = model.get_forecast(steps=12)
+        pred_ci = pred_uc.conf_int()
+        pred_unscaled_ind = pred_uc.predicted_mean
+        pred_unscaled = pred_unscaled_ind.reset_index(drop=True)
+    else:
+        pred_uc = model_package.get_forecast(steps=12)
+        pred_ci = pred_uc.conf_int()
+        pred_unscaled_ind = pred_uc.predicted_mean
+        pred_unscaled = pred_unscaled_ind.reset_index(drop=True)
     return date, pred_unscaled, sales, pred_ci, pred_unscaled_ind
-
 
 
 def books_forecast(filepath):
@@ -99,6 +104,25 @@ def future_dates(date):
     return future
 
 def plot(filepath):
+    date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_data(filepath)
+    fig = Figure(figsize=(25,15))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(sales, label='Observed', linewidth=5)
+    ax.plot(pred_unscaled_ind, label='Forecast', linewidth=5)
+    ax.fill_between(pred_ci.index,
+                    pred_ci.iloc[:, 0],
+                    pred_ci.iloc[:, 1], color='k', alpha=.25)
+    ax.set_title('Forecast for Next 12 months', fontsize=25)
+    ax.set_xlabel('Number of Observations', fontsize=20)
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(20)
+    ax.set_ylabel('Sales', fontsize=20)
+    ax.legend(prop={"size":20})
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def plot_package(filepath):
     date, pred_unscaled, sales, pred_ci, pred_unscaled_ind = model_data(filepath)
     fig = Figure(figsize=(25,15))
     ax = fig.add_subplot(1, 1, 1)
